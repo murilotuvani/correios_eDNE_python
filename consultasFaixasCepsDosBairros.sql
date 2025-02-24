ff14808961f645a3de59efc24b6abbcde824f3d4
Murilo Tuvani <murilo.tuvani@gmail.com>
2025-02-24 12:47:40 -0300

    Processando os bairros de Itu

--- consultasFaixasCepsDosBairros.sql
+++ consultasFaixasCepsDosBairros.sql
@@ -41,8 +41,51 @@ select a.loc_nu, a.loc_no, a.loc_in_sit, a.loc_in_tipo_loc tipo
       , b.bai_nu, b.bai_no, b.bai_no_abrev
       , c.fcb_cep_ini
       , c.fcb_cep_fim
-   from cep.rotas_externas_bairros reb left join cep.dne_bairro b on reb.bai_nu=b.bai_nu
-                                       left join dne_bairro_faixa c on c.bai_nu=b.bai_nu
-                                       left join dne_localidade a on b.loc_nu=a.loc_nu
-  where reb.Rotas is not null;
+   from cep.rotas_externas_bairros reb join cep.dne_bairro b on reb.bai_nu=b.bai_nu
+                                       join cep.dne_bairro_faixa c on c.bai_nu=b.bai_nu
+                                       join cep.dne_localidade a on b.loc_nu=a.loc_nu
+  where reb.Rotas is not null
+    AND c.fcb_cep_ini is not null;
+  
+select if(loc_nu=9260,12,0), Rotas, Rotas
+  from cep.rotas_externas_bairros reb
+ where reb.Rotas is not null
+group by loc_nu, Rotas
+order by loc_nu, Rotas;
+
+insert into autogeral.expe_rota_exte (loja_id, nume, nome, modo, cria_em, cria_por, alte_em, alte_por) 
+(select if(loc_nu=9260,12,0), Rotas, Rotas, 'IMEDIATA', now(), 'gabriel.ramos', now(), 'gabriel.ramos'
+  from cep.rotas_externas_bairros reb
+ where reb.Rotas is not null
+group by loc_nu, Rotas
+order by loc_nu, Rotas);
+
+
+select re.EXPE_ROTA_EXTE_ID
+      , concat_ws(' - Bairro N. ', b.bai_no, b.bai_nu) 
+      , c.fcb_cep_ini
+      , c.fcb_cep_fim
+      , now(), 'gabriel.ramos', now(), 'gabriel.ramos'
+   from cep.rotas_externas_bairros reb join cep.dne_bairro b on reb.bai_nu=b.bai_nu
+                                       join cep.dne_bairro_faixa c on c.bai_nu=b.bai_nu
+                                       join cep.dne_localidade a on b.loc_nu=a.loc_nu
+                                       join autogeral.expe_rota_exte re on reb.Rotas=re.nume
+  where reb.Rotas is not null
+    AND c.fcb_cep_ini is not null
+    and a.loc_nu=9260
+    and re.LOJA_ID=12;
  
+ insert into autogeral.expe_rota_exte_faix_cep (EXPE_ROTA_EXTE_ID, DSCR, CEP_INIC, CEP_FINA, cria_em, cria_por, alte_em, alte_por) 
+ select re.EXPE_ROTA_EXTE_ID
+      , concat_ws(' - Bairro N. ', b.bai_no, b.bai_nu) 
+      , c.fcb_cep_ini
+      , c.fcb_cep_fim
+      , now(), 'gabriel.ramos', now(), 'gabriel.ramos'
+   from cep.rotas_externas_bairros reb join cep.dne_bairro b on reb.bai_nu=b.bai_nu
+                                       join cep.dne_bairro_faixa c on c.bai_nu=b.bai_nu
+                                       join cep.dne_localidade a on b.loc_nu=a.loc_nu
+                                       join autogeral.expe_rota_exte re on reb.Rotas=re.nume
+  where reb.Rotas is not null
+    AND c.fcb_cep_ini is not null
+    and a.loc_nu=9260
+    and re.LOJA_ID=12;
